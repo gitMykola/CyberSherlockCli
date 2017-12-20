c731a1f6c74b54325a7d75696795ef2a2f9de4cb
Mykola <mykola_borodyn@ecoengineer.in.ua>
2017-12-20 22:56:49 +0200

    Start CyberSherlockCli styles

---
+++ src/app/tcomp/tcomp_component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-tcomp',
+    templateUrl: './tcomp_component.html',
+})
+export class TCompComponent implements OnInit {
+    constructor() { }
+    ngOnInit() { }
+}
